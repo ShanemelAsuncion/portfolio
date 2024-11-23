9567420c2f5cc25bba71e1fc54d50ee756dc117a
Shanemel Asuncion <shanemelasuncion@gmail.com>
2024-11-23 00:58:39 -0700

    Hero section design done
    TODO: Animations still need to add
    possibly add the social links

--- app/src/pages/Craft/Craft.jsx
+++ app/src/pages/Craft/Craft.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import './Craft.scss'
 
 function Craft() {
   return (
     <div className='container'>
+      <div className='hero-section'>
+        <div className='hero-section__main'>
+          <div className='hero-section__title'>
+            <h1>SHANEMEL</h1>
+            <h1>ASUNCION</h1>
+          </div>
+          <div className='hero-section__subtitle'>
+            <h4>Full Stack Developer</h4>
+            <h4>Based in Edmonton</h4>
+            <p>PORTFOLIO_2024</p>
+          </div>
+        </div>
 
-      Craft
+        <div className='hero-section__add'>
+          <p>&lt; SCROLL TO EXPLORE &gt;</p>
+        </div>
+      </div>
+
+      
+      <div className='craft-section'>
+        
+      </div>
     </div>
   )
 }
